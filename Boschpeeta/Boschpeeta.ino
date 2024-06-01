1a81201e2dfc11e6b613d4018e2a2414cc27520a
Guerra Santibanez Elisa (BGSW/EBS3.1-SX) <gse2ga@bosch.com>
2024-06-01 16:40:09 -0600

    Boschpeeta

--- Boschpeeta/Boschpeeta.ino
+++ Boschpeeta/Boschpeeta.ino
@@ -4,8 +4,45 @@
  *| |_| | |_| |____ ( (___| | | | |_| |  __/  __/ | | | |_|  |
  *|____/ \___/(____/ \____)_| |_| __ / \___|\___| |_|  \___|_|
  *                              |_|
+ *                              
+ * Elaborado por:
+ *    García García Andony Abdala
+ *    Guerra Santibañez Elisa
+ *    Moreno Paz Ximena
+ * 
+ * Equipo: MechMates (Verde)
+ *    
  */
- 
+
+//Se definen las variables
+
+#define MID_SPEED 100    
+#define HIGH_SPEED 120    
+#define LOW_SPEED 80    
+#define LONG_DELAY_TIME 70 
+#define DELAY_TIME 40 
+#define SHORT_DELAY_TIME 30 
+
+#define speedPinR 9   
+#define RightMotorDirPin1  22    
+#define RightMotorDirPin2  24                                    
+#define LeftMotorDirPin1  26   
+#define LeftMotorDirPin2  28
+#define speedPinL 10
+
+#define speedPinRB 
+#define RightMotorDirPin1B
+#define RightMotorDirPin2B  
+#define LeftMotorDirPin1B 7  
+#define LeftMotorDirPin2B 8 
+#define speedPinLB 12    
+
+#define sensor1   A4 
+#define sensor2   A3 
+#define sensor3   A2 
+#define sensor4   A1 
+#define sensor5   A0 
+
 void setup() {
   // put your setup code here, to run once:
 
